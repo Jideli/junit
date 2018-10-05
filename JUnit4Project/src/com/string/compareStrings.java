e74c51c01f22039043207acbc6f6a90ee6b1039c
Manon <Manon@manons-macbook-pro.home>
2018-10-05 09:48:38 +0200

    05.10.2018_1

--- JUnit4Project/src/com/string/compareStrings.java
+++ JUnit4Project/src/com/string/compareStrings.java
@@ -9,4 +9,37 @@ public class compareStrings {
 			return false;
 		}
 	}
+
+	public String concatString(String first, String second){
+		return first.concat(second);
+	}
+	
+	public int[] addOneToArray(int[] numbers) {
+		int length = numbers.length;
+		int output[] = new int[length];
+		for(int i=0; i<length;i++) {
+			output[i] = numbers[i]+1;
+		}
+		return output;
+	}
+	
+	public void performance() {
+		for(int i = 0; i < 100000; i++) {
+			
+		}
+		System.out.println("==Finish=====");
+	}
+	
+	public void exception (int number) {
+		int result = 10/number;
+		System.out.println("Result--> "+result);
+		
+	}
 }
+
+
+
+
+
+
+
--- JUnit4Project/test/com/string/compareStringsTest.java
+++ JUnit4Project/test/com/string/compareStringsTest.java
@@ -1,19 +1,55 @@
 
-
 package com.string;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
 public class compareStringsTest {
-
+	/*
+	// assertTrue and assertFalse methods --> Boolean methods
 	@Test
-	public void test() {
+	public void testCompareString() {
 		// create object of compareStrings class
 		compareStrings demo = new compareStrings();
-		
-		//assertTrue and assertFalse methods
 		// assertTrue("helo and hello fails",demo.compareString("hello", "hello"));
 		assertFalse("helo and hello fails",demo.compareString("hello", "hello"));
 	}
-
+	
+	
+	// assertEquals(expected, actual) method --> if actual, returns true
+	@Test
+	public void testConcatString() {
+		compareStrings demo = new compareStrings();
+	    assertEquals("HelloWorld", demo.concatString("Hello", "World")); // Hello is expectation and World is actual result
+	    assertEquals("HellWorld", demo.concatString("Hel", "World")); // Hello is expectation and World is actual result
+	    assertEquals("HiWorld", demo.concatString("Hi", "World")); // Hello is expectation and World is actual result
+	}
+	
+	// assertArrayEquals(expected, actual) method
+	@Test
+	public void testAddOneToArray() {
+		compareStrings demo = new compareStrings();
+		int expected[] = new int[]{3,8};
+		assertArrayEquals(expected, demo.addOneToArray(new int[]{2,7})); // this test will pass, add one to 2 and 7 will make 3 and 8
+		
+	}
+	
+	// assertArrayEquals(expected, actual) method
+	@Test(timeout = 30) // testing if it will time out in 30 milliseconds
+	public void testPerformance() {
+		compareStrings demo = new compareStrings();
+		demo.performance();
+	}*/
+		
+	
+	@Test (expected = ArithmeticException.class)
+	public void testException() {
+		compareStrings demo = new compareStrings();
+		demo.exception(0); // 0 will pass, 1 will fail
+	}
 }
+
+
+
+
+
+
